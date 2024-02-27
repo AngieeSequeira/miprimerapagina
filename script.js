3f12b7578c8c881bccd86e3f7b411ec25a86b3d1
AngieeSequeira <160974147+AngieeSequeira@users.noreply.github.com>
2024-02-26 22:01:55 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+//alert("Hola este es mi Javascript");
+
+//let nombre = "Angela";
+//var nombre1 = "Angela";
+//const nombre2 = "Angela";
+
+//console.log(nombre)
+//console.log(nombre1)
+//console.log(nombre2)
+
+//let titulo = document.querySelector(".logo.fuente.acento"); 
+
+//console.log(titulo);
+
+//titulo.innerHTML = "Hola";
+
+//SELECCION ELEMENTOS
+
+//let contenidoTitulo = "Nombre";
+
+//let Titulo = document.querySelector (".logo.fuente-titulo") Titulo.innerHTML = contenidoTitulo;
+
+//CONDICIONALES
+//let textoTitulo = Titulo.innerText;
+//console.log(textTitulo);
+
+//if(textTitulo=="Nombre"){ Titulo.innerHTML="otro";}
+
